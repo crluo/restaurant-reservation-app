7cf39b998211ba1898f34ee2930dcc864d84fbdd
Crystal Luo <crystalluo@Crystals-MacBook-Pro.local>
2021-11-18 15:13:40 -0500

    displays reservations for a specified date - no nav date buttons yet

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -27,10 +27,37 @@ function Dashboard({ date }) {
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for date {date}</h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+            <th scope="col">Party Size</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservations.map((reservation, index) => {
+            return (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{reservation.first_name}</td>
+                <td>{reservation.last_name}</td>
+                <td>{reservation.reservation_date}</td>
+                <td>{reservation.reservation_time}</td>
+                <td>{reservation.people}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* {JSON.stringify(reservations)} */}
     </main>
   );
 }
--- front-end/src/layout/ReservationForm.js
+++ front-end/src/layout/ReservationForm.js
@@ -13,7 +13,7 @@ function ReservationForm() {
         const abortController = new AbortController();
         async function addReservation() {
             try {
-                const newReservation = await createReservation( formData, abortController.signal );
+                const newReservation = await createReservation( {...formData, people: Number(formData.people)}, abortController.signal );
                 setFormData({});
                 history.push(`/dashboard?date=${newReservation.reservation_date}`);
             } catch (error) {
@@ -40,27 +40,27 @@ function ReservationForm() {
             <form onSubmit={ handleReservationSubmit }>
                 <div className="form-group">
                     <label for="exampleInputEmail1">First Name</label>
-                    <input name="first_name" required value={formData.first_name} onChange={handleReservationInputChange} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="John"/>
+                    <input name="first_name" required value={formData.first_name} onChange={handleReservationInputChange} type="text" className="form-control" id="first_name" aria-describedby="emailHelp" placeholder="John"/>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Last Name</label>
-                    <input name="last_name" required value={formData.last_name} onChange={handleReservationInputChange} type="text" className="form-control" id="exampleInputPassword1" placeholder="Doe"/>
+                    <input name="last_name" required value={formData.last_name} onChange={handleReservationInputChange} type="text" className="form-control" id="last_name" placeholder="Doe"/>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Mobile Number</label>
-                    <input name="mobile_number" required value={formData.mobile_number} onChange={handleReservationInputChange} type="text" className="form-control" id="exampleInputPassword1" placeholder="555-555-5555"/>
+                    <input name="mobile_number" required value={formData.mobile_number} onChange={handleReservationInputChange} type="text" className="form-control" id="mobile_number" placeholder="555-555-5555"/>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Reservation Date</label>
-                    <input name="reservation_date" required value={formData.reservation_date} onChange={handleReservationInputChange} type="date" className="form-control" id="exampleInputPassword1"/>
+                    <input name="reservation_date" required value={formData.reservation_date} onChange={handleReservationInputChange} type="date" className="form-control" id="reservation_date"/>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Reservation Time</label>
-                    <input name="reservation_time" required value={formData.reservation_time} onChange={handleReservationInputChange} type="time" className="form-control" id="exampleInputPassword1"/>
+                    <input name="reservation_time" required value={formData.reservation_time} onChange={handleReservationInputChange} type="time" className="form-control" id="reservation_time"/>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Party Size</label>
-                    <input name="people" required min={1} value={formData.people} onChange={handleReservationInputChange} type="number" className="form-control" id="exampleInputPassword1" placeholder="0"/>
+                    <input name="people" required min={1} value={formData.people} onChange={handleReservationInputChange} type="number" className="form-control" id="people" placeholder="0"/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
                 <button type="button" onClick={handleCancel} className="btn btn-primary">Cancel</button>
