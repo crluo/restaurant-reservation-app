97f1ffe605f6cedc6466c1dde8330516f70e2a58
Crystal Luo <crystalluo@Crystals-MacBook-Pro.local>
2021-12-10 17:18:05 -0500

    added comments to api

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -136,9 +136,7 @@ async function update(req, res) {
   const updatedReservation = await service.update(reservation);
   res.status(200).json({ data: updatedReservation })
 }
-/**
- * List handler for reservation resources
- */
+
 async function list(req, res) {
   const reservation_date = req.query.date;
   const filter = [ "finished", "cancelled" ];
